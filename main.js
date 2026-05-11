0a72de422a9516e67b991bf1ae08fdd4611c9c99
elderferreira067 <elder.silva2@estudante.ifms.edu.br>
2026-05-11 01:27:41 +0000

    cadastrar livros

--- main.js
+++ main.js
@@ -1,45 +1,55 @@
-const prompt = require('prompt-sync')();
-const Livro = require('./Livro');
+const prompt = require("prompt-sync")();
+const Livro = require("./Livro");
 const livros = [];
 
 let opcao = -1;
 
 while (opcao !== 0) {
-
-    console.log("\n--- BIBLIOTECA ---");
-    console.log("1 - Cadastrar livro");
-    console.log("2 - Listar livros");
-    console.log("3 - Alterar livro");
-    console.log("4 - Estatísticas");
-    console.log("0 - Sair");
+  console.log("\n--- BIBLIOTECA ---");
+  console.log("1 - Cadastrar livro");
+  console.log("2 - Listar livros");
+  console.log("3 - Alterar livro");
+  console.log("4 - Estatísticas");
+  console.log("0 - Sair");
 
   opcao = parseInt(prompt("Opcao: "));
 
-  if(isNaN(opcao)){
+  if (isNaN(opcao)) {
     console.log("Digite um número válido!");
     continue;
-  }  
+  }
 
-    switch (opcao){
-      case 1: 
-        break;
+  switch (opcao) {
+    case 1:
+      let titulo = prompt("Título: ");
+      let autor = prompt("Autor: ");
+      let genero = prompt("Gênero: ");
+      let anoPub = parseInt(prompt("Ano de Publicação: "));
+      let numPaginas = parseInt(prompt("Número de páginas: "));
 
-      case 2:
-        break;
+      let novoLivro = new Livro(titulo, autor, genero, anoPub, numPaginas);
 
-      case 3:
-        break;
+      livros.push(novoLivro);
 
-      case 4:
-        break;
+      console.log("Livro cadastrado!");
 
-      case 0:
-        console.log("Programa encerrado!");
-        break;
+      break;
 
-      default:
-        console.log("Opção inválida!");
-        break;
-    }
+    case 2:
+      break;
 
+    case 3:
+      break;
+
+    case 4:
+      break;
+
+    case 0:
+      console.log("Programa encerrado!");
+      break;
+
+    default:
+      console.log("Opção inválida!");
+      break;
+  }
 }
