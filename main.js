9ca3277239dd32282f7f32d9cdf342fd7d861646
elderferreira067 <elder.silva2@estudante.ifms.edu.br>
2026-05-11 00:52:10 +0000

    Menu Principal

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const prompt = require('prompt-sync')();
 const Livro = require('./Livro');
 const Livro = [];
+
+let opcao = -1;
+
+while (opcao !== 0) {
+
+    console.log("\n--- BIBLIOTECA ---");
+    console.log("1 - Cadastrar livro");
+    console.log("2 - Listar livros");
+    console.log("3 - Alterar livro");
+    console.log("4 - Estatísticas");
+    console.log("0 - Sair");
+
+}
